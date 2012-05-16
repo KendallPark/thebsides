498c5f39e4640df8648e12d45c7a0a22166f9450
Alex Rodriguez <arod@integritystl.com>
2012-05-16 12:05:28 -0500

    Updated Functions File & Added Local JS Scripts
    Added some useful functions for controlling JS and Admin Pages…

--- functions.php
+++ functions.php
@@ -3,6 +3,43 @@
  * @package WordPress
  * @subpackage #themeName
  */
+ 
+/**
+ * Includes scripts and styles (through wp_head())
+ * Register ALL of your scripts in this function to prevent conflicts...
+ */
+ 
+function registerScripts() {
+if(!is_admin()){
+	// Ckeck if Google's jQuery CDN is working and if it is load their jQuery library, if it is not, fallback to a local jQuery library script…
+	$url = 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'; // The URL to check against
+	$test_url = @fopen($url,'r'); // Test parameters
+	if($test_url !== false) { // Test if the URL exists
+    	function load_external_jQuery() { // Load external file
+        	wp_deregister_script( 'jquery' ); // Deregisters the default WordPress jQuery
+        	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'); // Register the external file. * be sure to update version numbers whenever there is a newer version of jQuery
+        	wp_enqueue_script('jquery'); // Enqueue the external file
+    	}
+		add_action('wp_enqueue_scripts', 'load_external_jQuery'); // Initiate the function
+	} else {
+    	function load_local_jQuery() {
+        	wp_deregister_script('jquery'); // Initiate the function
+        	wp_register_script('jquery', bloginfo('template_url').'/js/libs/jquery-1.7.1.min.js', __FILE__, false, '1.7.1', true); // Register the local file
+        	wp_enqueue_script('jquery'); // Enqueue the local file
+    	}
+		add_action('wp_enqueue_scripts', 'load_local_jQuery'); // Initiate the function
+	}
+    
+    // Load all other scripts you may need...
+    wp_register_script('modernizer', get_bloginfo('template_directory') .'/js/modernizr.js','jquery'); // Always include Modernizr when using HTML5
+    // *You should always redownload a minified/minimal version of Modernizr when pushing to a live site @ http://modernizr.com/download/
+    // *for dev we have loaded a minfied version of the full Modernizr library...
+    wp_enqueue_script('modernizer'); // Automatically load Modernizr into the header…
+    //wp_register_script( 'ui', get_bloginfo('template_directory') .'/js/jquery-ui-1.8.18.custom.min.js','jquery'); // Load only if you need to use the jQuery UI library
+	}
+}
+
+add_action( 'init', 'registerScripts' );
 
 if ( ! isset( $content_width ) )
 	$content_width = 640;
@@ -22,7 +59,7 @@ if ( ! function_exists( 'themeName_setup' ) ):
  * @uses register_default_headers() To register the default custom header images provided with the theme.
  * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
  *
- * @since Twenty Ten 1.0
+ * @since themeName 1.0
  */
 function themeName_setup() {
 
@@ -31,6 +68,8 @@ function themeName_setup() {
 
 	// This theme uses post thumbnails
 	add_theme_support( 'post-thumbnails' );
+	set_post_thumbnail_size( 200, 255, true ); // Normal post thumbnails  
+    add_image_size( 'screen-shot', 720, 540 ); // Full size screen
 
 	// Add default posts and comments RSS feed links to head
 	add_theme_support( 'automatic-feed-links' );
@@ -184,6 +223,120 @@ function themeName_comment( $comment, $args, $depth ) {
 endif;
 
 /**
+ * Register Custom Post Types
+ * The Custom Post Type is just a sample of how to create and register your own CPTs
+ */ 
+/*
+function register_splash_posts() {
+
+	$labels = array(
+		'name' => _x('Splash Posts', 'post type general name'),
+		'singular_name' => _x('Splash Page', 'post type singular name'),
+		'add_new' => _x('Add New', 'Splash Post'),
+		'add_new_item' => __('Add New Splash Post'),
+		'edit_item' => __('Edit Splash Post'),
+		'new_item' => __('New Splash Post'),
+		'view_item' => __('View Splash Post'),
+		'search_items' => __('Search Splash Post'),
+		'not_found' =>  __('No Splash Post Found'),
+		'not_found_in_trash' => __('No Splash Post Found In Trash'),
+		'parent_item_colon' => ''
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'query_var' => true,
+		//'menu_icon' => get_stylesheet_directory_uri() . '/eventicon.png', <- include this line to add custom icons for menu item
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'menu_position' => null,
+		'supports' => array('title','editor','thumbnail','custom-fields')
+	  );
+
+	register_post_type( 'splash' , $args );
+
+}
+
+add_action ('init', 'register_splash_posts');
+*/
+/**
+ * Register custom fields if needed
+ * The custom field below is just a sample of how to create and register custom fields…
+ */ 
+
+/*add_action("admin_init", "admin_init");
+ 
+function admin_init(){
+  add_meta_box("splash_testimonials", "Splash Page Testimonials", "splash_testimonials", "splash", "normal", "low");
+}
+
+function splash_testimonials() {
+  global $post;
+  $custom = get_post_custom($post->ID);
+  $htest = $custom["htest"][0];
+  $hname = $custom["hname"][0];
+  $ctest = $custom["ctest"][0];
+  $cname = $custom["cname"][0];
+  ?>
+  <p><label>Homeowner Testimonial:</label><br />
+  <textarea cols="80" rows="2" name="htest"><?php echo $htest; ?></textarea></p>
+  <p><label>Homeowner Name:</label><br />
+  <textarea cols="80" rows="1" name="hname"><?php echo $hname; ?></textarea></p>
+  <p><label>Commercial Owner Testimonial:</label><br />
+  <textarea cols="80" rows="2" name="ctest"><?php echo $ctest; ?></textarea></p> 
+  <p><label>Commercial Owner Name:</label><br />
+  <textarea cols="80" rows="1" name="cname"><?php echo $cname; ?></textarea></p> 
+  <?php
+}
+
+add_action('save_post', 'save_splash_testimonials');   
+  
+function save_splash_testimonials(){  
+    global $post;    
+  
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ){  
+        return $post_id;  
+    }else{  
+        update_post_meta($post->ID, "hname", $_POST["hname"]);
+        update_post_meta($post->ID, "htest", $_POST["htest"]);
+        update_post_meta($post->ID, "cname", $_POST["cname"]);
+        update_post_meta($post->ID, "ctest", $_POST["ctest"]);  
+    }  
+}
+*/
+
+/**
+ * Remove any unwanted widgets...
+ *
+ * WP_Widget_Pages                   = Pages Widget
+ * WP_Widget_Calendar                = Calendar Widget
+ * WP_Widget_Archives                = Archives Widget
+ * WP_Widget_Links                   = Links Widget
+ * WP_Widget_Meta                    = Meta Widget
+ * WP_Widget_Search                  = Search Widget
+ * WP_Widget_Text                    = Text Widget
+ * WP_Widget_Categories              = Categories Widget
+ * WP_Widget_Recent_Posts            = Recent Posts Widget
+ * WP_Widget_Recent_Comments         = Recent Comments Widget
+ * WP_Widget_RSS                     = RSS Widget
+ * WP_Widget_Tag_Cloud               = Tag Cloud Widget
+ * WP_Nav_Menu_Widget                = Menus Widget
+ */
+add_action('admin_menu', 'remove_menus');
+
+function remove_some_wp_widgets(){
+  unregister_widget('WP_Widget_Meta');
+  unregister_widget('WP_Widget_Calendar');
+  unregister_widget('WP_Widget_Recent_Comments');
+}
+
+add_action('widgets_init','remove_some_wp_widgets', 1);
+
+/**
  * Register widgetized areas, including two sidebars and four widget-ready columns in the footer.
  */
 function themeName_widgets_init() {
@@ -310,3 +463,124 @@ function themeName_posted_in() {
 	);
 }
 endif;
+
+/**
+ * Load custom login logo to login page
+ */
+function custom_login_logo() {
+  echo '<style type="text/css">
+    h1 a { background-image:url('.get_bloginfo('template_directory').'/img/login/logo.png) !important; }
+    </style>';
+}
+
+add_action('login_head', 'custom_login_logo');
+
+/**
+ * Customize the admin footer
+ */
+ function modify_footer_admin () {
+  echo 'Created by <a href="http://integritystl.com" target="_blank">Integrity</a>.';
+}
+
+add_filter('admin_footer_text', 'modify_footer_admin');
+
+/**
+ * Add a custom admin dashboard welcome widget
+ */
+function custom_dashboard_widget() {
+	echo "<p>Welcome to your custom WordPress site built by Integrity!</p>";
+}
+function add_custom_dashboard_widget() {
+	wp_add_dashboard_widget('custom_dashboard_widget', 'Integrity Welcomes You To WordPress!', 'custom_dashboard_widget');
+}
+add_action('wp_dashboard_setup', 'add_custom_dashboard_widget');
+
+/**
+ * Remove useless admin dashboard widgets
+ */
+function disable_default_dashboard_widgets() {
+
+	remove_meta_box('dashboard_right_now', 'dashboard', 'core');
+	remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
+	remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
+	remove_meta_box('dashboard_plugins', 'dashboard', 'core');
+	remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
+	remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core');
+	remove_meta_box('dashboard_primary', 'dashboard', 'core');
+	remove_meta_box('dashboard_secondary', 'dashboard', 'core');
+}
+add_action('admin_menu', 'disable_default_dashboard_widgets');
+
+/**
+ * Remove any unnecessary admin menus & sub-menus
+ */
+
+function remove_menus () {
+global $menu;
+	$restricted = array(__('Links'));
+	// Additional options - > __('Dashboard'), __('Pages'), , __('Comments'), __('Media')
+	end ($menu);
+	while (prev($menu)){
+		$value = explode(' ',$menu[key($menu)][0]);
+		if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+	}
+}
+
+add_action('admin_menu', 'remove_menus');
+
+function remove_submenus() {
+  global $submenu;
+  //Dashboard menu
+  //unset($submenu['index.php'][10]); // Removes Updates
+  //Posts menu
+  //unset($submenu['edit.php'][5]); // Leads to listing of available posts to edit
+  //unset($submenu['edit.php'][10]); // Add new post
+  //unset($submenu['edit.php'][15]); // Remove categories
+  unset($submenu['edit.php'][16]); // Removes Post Tags
+  //Media Menu
+  //unset($submenu['upload.php'][5]); // View the Media library
+  //unset($submenu['upload.php'][10]); // Add to Media library
+  //Links Menu
+  //unset($submenu['link-manager.php'][5]); // Link manager
+  //unset($submenu['link-manager.php'][10]); // Add new link
+  //unset($submenu['link-manager.php'][15]); // Link Categories
+  //Pages Menu
+  //unset($submenu['edit.php?post_type=page'][5]); // The Pages listing
+  unset($submenu['edit.php?post_type=page'][10]); // Add New page
+  //Appearance Menu
+  unset($submenu['themes.php'][5]); // Removes 'Themes'
+  //unset($submenu['themes.php'][7]); // Widgets
+  unset($submenu['themes.php'][15]); // Removes Theme Installer tab
+  //Plugins Menu
+  //unset($submenu['plugins.php'][5]); // Plugin Manager
+  //unset($submenu['plugins.php'][10]); // Add New Plugins
+  unset($submenu['plugins.php'][15]); // Plugin Editor
+  //Users Menu
+  //unset($submenu['users.php'][5]); // Users list
+  //unset($submenu['users.php'][10]); // Add new user
+  //unset($submenu['users.php'][15]); // Edit your profile
+  //Tools Menu
+  //unset($submenu['tools.php'][5]); // Tools area
+  //unset($submenu['tools.php'][10]); // Import
+  //unset($submenu['tools.php'][15]); // Export
+  //unset($submenu['tools.php'][20]); // Upgrade plugins and core files
+  //Settings Menu
+  //unset($submenu['options-general.php'][10]); // General Options
+  //unset($submenu['options-general.php'][15]); // Writing
+  //unset($submenu['options-general.php'][20]); // Reading
+  //unset($submenu['options-general.php'][25]); // Discussion
+  //unset($submenu['options-general.php'][30]); // Media
+  //unset($submenu['options-general.php'][35]); // Privacy
+  //unset($submenu['options-general.php'][40]); // Permalinks
+  //unset($submenu['options-general.php'][45]); // Misc
+}
+add_action('admin_menu', 'remove_submenus');
+
+// Remove Theme Editor
+
+add_action('admin_init', 'my_remove_menu_elements', 102);
+
+function my_remove_menu_elements()
+{
+	remove_submenu_page( 'themes.php', 'theme-editor.php' );
+}
