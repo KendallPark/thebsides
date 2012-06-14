6b78788feeed0cafe2cdaa23a72c5f43ddc040d4
Joshua Ray <joshua@integritystl.com>
2012-06-14 08:55:14 -0500

    refactoring theme setup and widget and sidebar init

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
 
 
 // Includes
-require_once('includes/post-types.php');
+require_once('includes/post-types/post-types.php');
 require_once('includes/user-settings.php');
 require_once('includes/scripts.php');
 require_once('includes/theme-setup.php');
@@ -14,4 +14,4 @@ require_once('includes/admin-setup.php');
 require_once('includes/post-types.php');
 require_once('includes/widgets-containers.php');
 require_once('includes/widgets-elements.php');
-require_once( dirname( __FILE__ ) . '/theme-options.php' );
+require_once('theme-options.php' );
