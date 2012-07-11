0043bd7c444f8c81b1295b8568b2802a04901250
Joshua Ray <joshua@integritystl.com>
2012-07-11 13:43:28 -0500

    comments for functions

--- functions.php
+++ functions.php
@@ -6,12 +6,25 @@
  * @subpackage #themeName
  */
 
+// Always enqueue your javascript 
 require_once('includes/scripts.php');
+
+// Customizes the admin 
 require_once('includes/admin-setup.php');
+
+// Core functions of our theme
 require_once('includes/theme-setup.php');
+
+// Where all custom post types are registered
 require_once('includes/custom-post-types/sample-post-types.php');
+
+// Where shortcodes are defined 
 require_once('includes/shortcodes.php');
+
+// Where all of your custom widgets are created
 require_once('includes/register-widgets.php');
+
+// Where all sidebars are created for unique pages 
 require_once('includes/register-sidebars.php');
 
 ?>
