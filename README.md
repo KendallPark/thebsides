08a74f181373258bdefa046fdcbf56d78f7b8276
Alex Rodriguez <arod@integritystl.com>
2012-07-12 13:56:00 -0500

    Added a sample custom taxonomy

--- README.md
+++ README.md
@@ -34,16 +34,19 @@
 	
 ##Change Log##
 
+* 07-12-12 (AROD) Added sample taxonmy and taxonmy page template.
+
 * 07-11-12 (AROD) Added this README.md file and moved non standard template files author.php, attachment.php, comments.php into an "extra-templates" folder where you can pull them if needed. If you don't need any of them just delete that folder before launching... Added a default favicon
 
 ##Feature Requests##
 
-* Build a sample custom taxonomy template that can be easily duplicated
+* Add custom post type icon images from designers...
 
 * Develop "standard" tag, category, archive, 404, and serach templates for simple customization
 
 * Make footer stick to the bottom of the page (http://ryanfait.com/resources/footer-stick-to-bottom-of-page/)
 
+X * Build a sample custom taxonomy template that can be easily duplicated
 
 
 
--- functions.php
+++ functions.php
@@ -9,7 +9,9 @@
 require_once('includes/scripts.php');
 require_once('includes/admin-setup.php');
 require_once('includes/theme-setup.php');
-require_once('includes/custom-post-types/sample-post-types.php');
+require_once('includes/custom-post-types/post-types.php');
+require_once('includes/custom-meta-fields/meta-fields.php');
+require_once('includes/custom-taxonomies/taxonomy.php');
 require_once('includes/shortcodes.php');
 require_once('includes/register-widgets.php');
 require_once('includes/register-sidebars.php');
@@ -21,7 +23,7 @@ require_once('includes/user-settings.php');
  *
  * More info @ http://leemason.github.com/NHP-Theme-Options-Framework/
  *
- * If you are not going to need this framework, remove the "theme-options.php" and "options" folder for less bloat! :)
+ * If you are not going to need this framework, remove the "theme-options.php" and "options/" folder for less bloat! :)
  *
  */
 
