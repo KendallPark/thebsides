94224fbedfe63feeb7a3f3bbecf1eb7ba922a13a
Alex Rodriguez <arod@integritystl.com>
2012-07-30 09:38:59 -0500

    Added front-page.php to extra-templates folder

--- extra-templates/home.php
+++ extra-templates/home.php
@@ -1,4 +1,7 @@
-<?php //This is going to be unique to almost every website... No need to have a "standard" template. 
+<?php 
+
+// When Posts page is set in the Front page displays section of Settings -> Reading
+//This is going to be unique to almost every website... No need to have a "standard" template. 
 	get_header();
 ?>
 
